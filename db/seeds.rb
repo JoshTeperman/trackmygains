7d0fe2c83495785628e30354737d7d4d49b1a7d9
Josh Teperman <Josh.teperman@weployapp.com>
2020-06-07 23:25:15 +1000

    update seeds with exercise type

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,36 @@
 p 'Seeding Workouts...'
 
-# current_workout
+# exercise types
+bench_press = ExerciseType.create!(name: 'Bench Press', targets: ['chest', 'shoulders', 'triceps'], category: :resistance)
+barbell_squat = ExerciseType.create!(name: 'Barbell Squat', targets: ['legs'], category: :resistance)
+rowing_machine = ExerciseType.create!(name: 'Rowing Machine', targets: ['back'], category: :cardio)
 
+# current_workout
 current_workout = Workout.create!(start_time: Time.now)
 
-# completed workouts
-completed_workout = Workout.new(start_time: 24.hours.ago, finish_time: 23.hours.ago, state: 'completed')
-completed_workout_cardio_exercise = CardioExercise.create!(
-  name: 'elliptical',
+# completed workout 1
+completed_workout = Workout.create!(start_time: 24.hours.ago, finish_time: 23.hours.ago, state: 'completed')
+completed_workout_cardio_exercise = CardioExercise.new(
   total_time: 15,
   start_time: 24.hours.ago,
   finish_time: 24.hours.ago + 15.minutes,
   distance: 2.0
 )
-completed_workout_resistance_exercise_1 = ResistanceExercise.create!(
-  name: 'sit-up',
-  target: 'core'
-)
+completed_workout_cardio_exercise.exercise_type = rowing_machine
+completed_workout_cardio_exercise.save!
+
+completed_workout_resistance_exercise_1 = ResistanceExercise.new
+completed_workout_resistance_exercise_1.exercise_type = barbell_squat
+completed_workout_resistance_exercise_1.save!
 
 completed_workout.exercises << Exercise.create!(exercisable: completed_workout_cardio_exercise)
 completed_workout.exercises << Exercise.create!(exercisable: completed_workout_resistance_exercise_1)
 
-completed_workout.save!
-
-
+# completed workout 2
 completed_workout_2 = Workout.create!(start_time: 48.hours.ago, finish_time: 45.hours.ago, state: 'completed')
-completed_workout_2_exercise = ResistanceExercise.create!(
-  name: 'deadlift',
-  target: 'back'
-)
+completed_workout_2_exercise = ResistanceExercise.new
+completed_workout_2_exercise.exercise_type = bench_press
+completed_workout_2_exercise.save!
+
 completed_workout_2.exercises << Exercise.create!(exercisable: completed_workout_2_exercise)
 
