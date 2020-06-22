74ee6f587fc681fbc4d87b34d212be522bbeab7a
Josh Teperman <Josh.teperman@weployapp.com>
2020-06-23 00:04:17 +1000

    link each preview component to exercise show page

--- app/components/workouts/exercise_preview_component.rb
+++ app/components/workouts/exercise_preview_component.rb
@@ -1,11 +1,33 @@
 module Workouts
   class ExercisePreviewComponent < ApplicationComponent
-    attr_reader :category, :name
-    def initialize(exercise:)
+    attr_reader :name, :path
+
+    def initialize(exercise:, workout:)
       @exercise = exercise
-      @exercise_type = exercise.exercise_type
-      @name = @exercise_type.name
-      @category = @exercise_type.category
+      @workout = workout
+      @name = exercise.exercise_type.name
+      @path = set_path
+    end
+
+    private
+
+    def set_path
+      case exercise.exercisable_type
+      when 'ResistanceExercise'
+        Rails.application.routes.url_helpers.workout_resistance_exercise_path(workout, exercise.exercisable_id)
+      when 'CardioExercise'
+        Rails.application.routes.url_helpers.workout_cardio_exercise_path(workout, exercise.exercisable_id)
+      when 'CalisthenicsExercise'
+        Rails.application.routes.url_helpers.workout_calisthenics_exercise_path(workout, exercise.exercisable_id)
+      end
+    end
+
+    def workout
+      @workout
+    end
+
+    def exercise
+      @exercise
     end
   end
 end
