75f3272f735ac76a99565cdb14edfd0fb2c6a8d6
Josh Teperman <Josh.teperman@weployapp.com>
2020-07-18 12:54:34 +1000

    exercisable dependent: :destroy

--- app/models/concerns/exercisable.rb
+++ app/models/concerns/exercisable.rb
@@ -2,6 +2,6 @@ module Exercisable
   extend ActiveSupport::Concern
 
   included do
-    has_one :exercise, as: :exercisable
+    has_one :exercise, as: :exercisable, dependent: :destroy
   end
 end
