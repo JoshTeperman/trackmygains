3d610eb7da45c881623b243ae4b960b33bf08970
Josh Teperman <Josh.teperman@weployapp.com>
2020-07-20 00:26:56 +1000

    render_svg method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def render_svg(name, options = {})
+    size_classes = {
+      'xsmall': '2rem * 2rem',
+      'small': '3rem * 3rem',
+      'medium': '6rem * 6rem',
+      'large': '8rem * 8rem'
+    }
+
+    options[:title] = name.underscore.humanize
+    options[:class] = options.fetch(:class, 'fill-current text-green-900')
+    options[:aria] = true
+    options[:nocomment] = true
+    options[:size] = size_classes[options.fetch(:size, 'small').to_sym]
+
+    file_name = "#{name}.svg"
+
+    inline_svg_tag(file_name, options)
+  end
 end
