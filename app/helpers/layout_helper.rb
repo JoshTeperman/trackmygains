a63429bc79fe84e39097384ad27ddcce875044d8
Josh Teperman <Josh.teperman@weployapp.com>
2020-08-23 14:30:58 +1000

    change app name to Undo

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,6 +1,6 @@
 module LayoutHelper
   def page_title
-    app_title = 'Track My Gains'
+    app_title = 'Undo'
     prefix = content_for?(:page_title) ? "#{content_for(:page_title)} | " : ''
     content_tag(:title, prefix.concat(app_title))
   end
