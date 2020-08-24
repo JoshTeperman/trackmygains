ba66b9aa91ff38d873774a4c843c6697a6318a59
Josh Teperman <Josh.teperman@weployapp.com>
2020-08-24 23:50:45 +1000

    calculate exercise summary

--- app/components/workouts/summary_component.rb
+++ app/components/workouts/summary_component.rb
@@ -17,4 +17,37 @@ class Workouts::SummaryComponent < ApplicationComponent
     minutes = (seconds / 60).to_i
     "#{minutes} minutes"
   end
+
+  def exercise_detail(exercise)
+    case exercise.exercisable_type
+    when 'CalisthenicsExercise'
+      '5 sets, 100 reps'
+    when 'ResistanceExercise'
+      '5 sets, 75kg max'
+    when 'CardioExercise'
+      "2km in 10'12\""
+    end
+  end
+
+  def total_distance
+    return if workout.cardio_exercises.blank?
+
+    distances = workout.cardio_exercises.pluck(:distance).compact
+    return if distances.blank?
+
+    total = distances .map { |d| BigDecimal(d, 2) }
+                      .reduce(:+)
+
+    "#{total}km"
+  end
+
+  def total_weight
+    return if workout.resistance_exercises.blank?
+
+    '843kg'
+  end
+
+  def total_sets
+    '15 sets'
+  end
 end
